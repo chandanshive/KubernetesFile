b8baaefe3acf8b99c263b25a06ff094caa746c6d
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-07-14 13:24:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,26 +14,3 @@ resource "aws_s3_bucket" "bucket" {
     Name        = "My bucket"
   }
 }
-
-provider "docker" {
-}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:1.11-alpine"
-  keep_locally = "true"
-}
-
-resource "docker_container" "nginx-server" {
-  name  = "nginx-server"
-  image = docker_image.nginx.latest
-  ports {
-    internal = 80
-  }
-
-  volumes {
-    container_path = "/usr/share/nginx/html"
-    host_path      = "/tmp/"
-    read_only      = true
-
-  }
-}
